a754c727dbb083d18cf3608362f6bbf4e7031849
Gabriel Kobayashi <gabrielkobayashi11@gmail.com>
2026-01-12 21:29:57 -0300

    Excessão personalizada e classe de inicialização

---
+++ src/main/java/com/github/gabriel/user_manager/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.github.gabriel.user_manager.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.github.gabriel.user_manager.entity.User;
+import com.github.gabriel.user_manager.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User u1 = new User(null, "João Victor", "joao@gmail.com");
+		User u2 = new User(null, "Maria Clara", "maria@gmail.com");
+		User u3 = new User(null, "Carlos Alberto", "carlos@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(u1, u2, u3));
+	}
+
+}
---
+++ src/main/java/com/github/gabriel/user_manager/exception/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package com.github.gabriel.user_manager.exception;
+
+public class UserNotFoundException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public UserNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
