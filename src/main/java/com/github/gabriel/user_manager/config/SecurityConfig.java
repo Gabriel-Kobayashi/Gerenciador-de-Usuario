8ffa5b3c0c51010ffd94784601a8eadab2d564cd
Gabriel Kobayashi <gabrielkobayashi11@gmail.com>
2026-01-19 14:22:26 -0300

    fix: ajustar retorno 401 para rotas protegidas

--- src/main/java/com/github/gabriel/user_manager/config/SecurityConfig.java
+++ src/main/java/com/github/gabriel/user_manager/config/SecurityConfig.java
@@ -2,18 +2,19 @@ package com.github.gabriel.user_manager.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.github.gabriel.user_manager.security.SecurityFilter;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableWebSecurity
@@ -23,22 +24,28 @@ public class SecurityConfig {
 	
 	public SecurityConfig(SecurityFilter securityFilter) {
 		this.securityFilter = securityFilter;
-	}
+	} 
 	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 	    return http
 	        .csrf(csrf -> csrf.disable())
 	        .sessionManagement(session ->
-	        		session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-	        		)
-	        .authorizeHttpRequests(auth -> auth
-	        		.requestMatchers("/auth/login", "/auth/register").permitAll()
-	        		.anyRequest().authenticated()
+	            session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 	        )
-	        .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class).build();    				
-	};
-	
+	        .exceptionHandling(exception -> exception
+	            .authenticationEntryPoint((request, response, authException) -> {
+	                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+	            })
+	        )
+	        .authorizeHttpRequests(auth -> auth
+	            .requestMatchers("/auth/login", "/auth/register").permitAll()
+	            .anyRequest().authenticated()
+	        )
+	        .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+	        .build();
+	}
+
 	@Bean
 	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
 		return authenticationConfiguration.getAuthenticationManager();
